c8152dafbb40c9ffc7b3ae5921380898d2fad17c
张镇鸿 <15521280455@163.com>
2018-01-21 22:49:58 +0800

    激活码生成
    生成激活码并将结果写入到文件里

--- 0002.py
+++ 0002.py
@@ -8,14 +8,12 @@ def gen_code(length=8):
     for i in list(range(10)):
         code_list.append(str(i))
             #print i
-        print(code_list)
     for i in list(range(65, 91)):
         code_list.append(chr(i))
-        print(code_list)
             #print chr(i)
     for i in list(range(97, 123)):
         code_list.append(chr(i))
-        print(code_list)
+
 
     myslice = random.sample(code_list, length) #可以从指定的序列中，随机的截取指定长度的片断，不作原地修改
     veri_code = ''.join(myslice) #将字符序列进行连接
@@ -23,8 +21,13 @@ def gen_code(length=8):
     return veri_code
 
 if __name__ == "__main__":
-  #  for i in range(10):
-        print(gen_code(8))
 
+    myPath = "C:\\Users\\Lenovo\\Desktop\\"    #桌面路径
+    outputFile = "output.txt"
+    f = open(myPath + outputFile, "w")
+    num = int(input("请输入你要产生的激活码数量："))
+    for i in range(num):
+        f.write(gen_code(8) + "\n")           #将激活码写入output.txt里
+    f.close()
 
 
